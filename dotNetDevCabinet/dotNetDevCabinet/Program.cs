0b0f4fe8b3380867ede59cbcf24d3bb082fb802a
Alejandro Herrera <lherreradz@gmail.com>
2021-01-10 15:59:19 -0500

    Insert TestDotNet5

---
+++ dotNetDevCabinet/dotNetDevCabinet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestDotNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensaje mensaje = new Mensaje { saludo = "Hola" };
+            var user = new User("Alejo");
+            Console.WriteLine($"{mensaje.saludo} {user.Name}");
+        }
+
+        private record User(string Name);
+
+        private class Mensaje
+        {
+            public string saludo { get; set; }
+        }
+    }
+}
